8bd2a54164106cf166b5e121d506e788ad96d4be
lewisnewton <lnewton889@gmail.com>
2022-07-25 13:37:59 +0100

    Update projector
    This gives options to finds screens, create a projector, change the screen it 'projects' too, get the dimensions, test itself with a white image, and project any image :)
    
    Co-Authored-By: SamFerrisUoN <107254753+SamFerrisUoN@users.noreply.github.com>

--- projector/_projector.py
+++ projector/_projector.py
@@ -2,8 +2,60 @@
 
 import numpy as np
 import screeninfo as si
+import cv2
+
+def screens():
+    
+    screen_ids = si.get_monitors()
+    print(screen_ids)        
+    return screen_ids
+
 
 class Projection():
     
-    def __init__(self):
-        self
+    def __init__(self,screen_id):
+        self.screen_id = screen_id
+        self.screen = si.get_monitors()[self.screen_id]       
+        self.width = self.screen.width
+        self.height = self.screen.height
+        self.x = self.screen.x
+        self.y = self.screen.y
+        
+    def change_screen(self,new_screen_id):
+        self.screen_id = new_screen_id
+        self.screen = si.get_monitors()[self.screen_id]
+        self.width = self.screen.width
+        self.height = self.screen.height
+        self.x = self.screen.x
+        self.y = self.screen.y
+    
+    def size():
+        dimension = [self.width, self.height]
+        return dimension
+        
+    def project_test(self):
+        img =  np.ones([self.width,self.height])
+        
+        window_name = "project_test"
+        
+        cv2.namedWindow(window_name,cv2.WND_PROP_FULLSCREEN)
+        cv2.moveWindow(window_name, self.x - 1,self.y - 1)
+        cv2.setWindowProperty(window_name,cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+        cv2.imshow(window_name ,img)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+    
+    def project(self, image, image_name):
+        
+        if image_name:
+            window_name = "image"
+        else:
+            window_name = image_name
+        
+        cv2.namedWindow(window_name,cv2.WND_PROP_FULLSCREEN)
+        cv2.moveWindow(window_name, self.x - 1,self.y - 1)
+        cv2.setWindowProperty(window_name,cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+        cv2.imshow(window_name ,image)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+    
